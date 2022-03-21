ca51061d73969f2536a9e47765cdff21e5edceae
Jerry Yang <efJerryYang@outlook.com>
2022-03-21 23:01:15 +0800

    fix: shift Drag with Move to avoid bug sudden move

--- src/edu/hitsz/application/HeroController.java
+++ src/edu/hitsz/application/HeroController.java
@@ -16,18 +16,18 @@ public class HeroController {
     private HeroAircraft heroAircraft;
     private MouseAdapter mouseAdapter;
 
-    public HeroController(Game game, HeroAircraft heroAircraft){
+    public HeroController(Game game, HeroAircraft heroAircraft) {
         this.game = game;
         this.heroAircraft = heroAircraft;
 
         mouseAdapter = new MouseAdapter() {
             @Override
-            public void mouseDragged(MouseEvent e) {
-                super.mouseDragged(e);
-                // my Todo: 瞬移bug
+            public void mouseMoved(MouseEvent e) {
+                super.mouseMoved(e);
+                // (DONE) my Todo: 瞬移bug, 点击鼠标会出现瞬移, 把Drag改为Move
                 int x = e.getX();
                 int y = e.getY();
-                if ( x<0 || x>Main.WINDOW_WIDTH || y<0 || y>Main.WINDOW_HEIGHT){
+                if (x < 0 || x > Main.WINDOW_WIDTH || y < 0 || y > Main.WINDOW_HEIGHT) {
                     // 防止超出边界
                     return;
                 }
